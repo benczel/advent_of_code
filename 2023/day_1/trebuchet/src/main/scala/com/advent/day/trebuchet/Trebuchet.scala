9efd4f98696b95284dbeeddcadf926742b92023e
Bencze László <laci@Bencze-MacBook-Air.local>
2024-11-25 20:57:00 +0100

    use string buffer

--- 2023/day_1/trebuchet/src/main/scala/com/advent/day/trebuchet/Trebuchet.scala
+++ 2023/day_1/trebuchet/src/main/scala/com/advent/day/trebuchet/Trebuchet.scala
@@ -2,21 +2,23 @@ package com.advent.day.trebuchet
 
 import scala.io._
 
-def keepDigitalCharOnly(line:String):String = {
+private def keepDigitalCharOnly(line:String):String = {
   line.replaceAll("\\D", "")
 }
 
-def processCode(code:String):String = {
+private def processCode(code:String):String = {
   code.length match
     case 1 => code ++ code
     case 2 => code
-    case _ => code.toCharArray.head.toString ++ code.toCharArray.last.toString
+    case _ => StringBuffer(2).append(code.toCharArray.head).append(code.toCharArray.last).toString
 }
 
 @main def read():Unit = {
-  println(Source.fromResource("input.txt").getLines()
+  val sum = Source.fromResource("input.txt").getLines()
     .map(keepDigitalCharOnly)
     .map(processCode)
     .map(_.toInt)
-    .sum)
+    .sum
+
+  println(sum)
 }
